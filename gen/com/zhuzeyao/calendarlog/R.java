730202eea48e507be1137603b27d30a867933b66
tyzhangxj <tyzhangxj@gmail.com>
2016-02-19 14:53:05 +0800

    b

--- gen/com/zhuzeyao/calendarlog/R.java
+++ gen/com/zhuzeyao/calendarlog/R.java
@@ -16,7 +16,8 @@ public final class R {
     public static final class id {
         public static final int linearLayout1=0x7f060000;
         public static final int textView1=0x7f060001;
-        public static final int textView2=0x7f060002;
+        public static final int textView2=0x7f060003;
+        public static final int textView3=0x7f060002;
     }
     public static final class layout {
         public static final int cal=0x7f030000;
